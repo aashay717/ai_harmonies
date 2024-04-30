de54a70596b18086cb38d82a6df0e8bd7f034a0f
Aashay Raj <127934378+aashay717@users.noreply.github.com>
2024-05-01 00:30:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -110,3 +110,90 @@ process.on('SIGTERM', async () => {
     await closeMongoDBConnection();
     process.exit();
 });
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const Razorpay = require('razorpay');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 9900;
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+}
+
+async function closeMongoDBConnection() {
+    try {
+        await client.close();
+        console.log('Connection to MongoDB closed');
+    } catch (err) {
+        console.error('Error closing MongoDB connection:', err);
+    }
+}
+
+connectToMongoDB();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+app.use(express.static('public'));
+
+app.post('/create/order', async (req, res) => {
+    const options = {
+        amount: req.body.amount,
+        currency: 'INR',
+        receipt: 'order_rcptid_' + Math.floor(Date.now() / 1000),
+        payment_capture: '1',
+    };
+
+    try {
+        const response = await razorpay.orders.create(options);
+        res.json({ orderId: response.id });
+    } catch (error) {
+        console.error('Error creating order:', error);
+        res.status(500).json({ error: 'Failed to create order' });
+    }
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.get('/', (req, res) => {
+    const username = req.query.username || '';
+    res.render('index', { username });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+// Close MongoDB connection when the app is terminated
+process.on('SIGINT', async () => {
+    await closeMongoDBConnection();
+    process.exit();
+});
+
+process.on('SIGTERM', async () => {
+    await closeMongoDBConnection();
+    process.exit();
+});
